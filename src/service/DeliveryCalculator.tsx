43d4e83c09ed11198eb19c3cb9c65aeb2ae846d1
raneem-yad <eng.raneem.yad@gmail.com>
2024-01-29 19:17:26 +0100

    enhance the  bulk fee calculation

--- src/service/DeliveryCalculator.tsx
+++ src/service/DeliveryCalculator.tsx
@@ -1,6 +1,7 @@
 const hasSurchargeValue = (cartValue: number): boolean => cartValue > 0 && cartValue < 10
 const calculateSurchargeValue = (cartValue: number): number => 10 - cartValue
-export const getSurchargeValue = (cartValue: number): number => hasSurchargeValue(cartValue) ? calculateSurchargeValue(cartValue) : 0
+export const getSurchargeValue = (cartValue: number): number =>
+    hasSurchargeValue(cartValue) ? calculateSurchargeValue(cartValue) : 0
 
 
 // surcharge with number of items
@@ -10,12 +11,13 @@ const calculateSurchargePerNumberOfItemsValue = (numOfItems: number): number => 
 const getSurchargePerNumberOfItemsValue = (numOfItems: number): number =>
     hasSurchargePerNumberOfItemsValue(numOfItems) ? calculateSurchargePerNumberOfItemsValue(numOfItems) : 0;
 
+
 // add bulk fee according to number of items
 const hasBulkFeeValue = (numOfItems: number): boolean => numOfItems > 12
-const calculateBulkFeeValue = (preSurchargeValue: number): number => preSurchargeValue + 1.20;
-const getBulkFeeValue = (numOfItems: number, preSurchargeValue: number): number =>
-    hasBulkFeeValue(numOfItems) ? calculateBulkFeeValue(preSurchargeValue) : preSurchargeValue;
+const calculateBulkFeeValue = (): number => 1.20;
+const getBulkFeeValue = (numOfItems: number): number =>
+    hasBulkFeeValue(numOfItems) ? calculateBulkFeeValue() : 0;
 
-//combine the rule 3 with rule 2 
+// surcharge according to number of items + bulk fee according to number of items
 export const getNumberOfItemsFeeValue = (numOfItems: number): number =>
-    getBulkFeeValue(numOfItems, getSurchargePerNumberOfItemsValue(numOfItems));
+    getBulkFeeValue(numOfItems) + getSurchargePerNumberOfItemsValue(numOfItems);
